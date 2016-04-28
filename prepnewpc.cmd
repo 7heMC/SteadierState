f945564e1d704645720f6fa1efae49784c709b1d
M. Cecil Dietz <mcecildietz@gmail.com>
2016-04-28 11:25:30 -0400

    Added compatibility with Windows 10

--- prepnewpc.cmd
+++ prepnewpc.cmd
@@ -1,9 +1,11 @@
 @echo off
-REM 
 REM
 REM Check that we're running from the root of the boot device
 REM Use the pseudo-variable ~d0 to get the job done
+REM exdrive = external drive where the vhd is stored
+REM
 set drive=%~d0
+set exdrive=%1
 if not x%drive%x==xX:x goto :pleasebootfromUSBfirst
 %drive%
 cd \
@@ -16,6 +18,7 @@ REM onto the hard disk, I need access to non-expanded
 REM version of the \sources\boot.wim image.  This tries to
 REM find that by checking drive letters C: through P: for 
 REM the sources\boot.wim file.)
+REM
 set realdrive=none
 if exist c:\sources\boot.wim set realdrive=c:
 if exist d:\sources\boot.wim set realdrive=d:
@@ -38,6 +41,7 @@ echo to exit.  Please ensure that you're running this command file from  WinPE-e
 echo USB stick or CD that you have booted your PC from.
 echo.
 goto :badend
+
 :foundrealdrive
 echo.
 echo Found the USB stick/CD's native drive=%realdrive%
@@ -59,65 +63,79 @@ echo Q: and W:.  I can't do the job without a free drive letter, so I've got
 echo to stop.
 echo.
 goto :badend
+
 :foundtdrive
 echo Found an available drive letter=%tdrive%:
 echo (Those are both good news.)
 echo.
 REM
-REM Then check for an onboard copy of imagex.
+REM Find an available drive letter for the vhd drive
 REM
-if exist x:\windows\system32\imagex.exe goto  :foundimagex
-
+set vdrive=none
+if not exist W:\ if not %tdrive%==w set vdrive=w
+if not exist V:\ if not %tdrive%==v set vdrive=v
+if not exist U:\ if not %tdrive%==u set vdrive=u
+if not exist T:\ if not %tdrive%==t set vdrive=t
+if not exist S:\ if not %tdrive%==s set vdrive=s
+if not exist R:\ if not %tdrive%==r set vdrive=r
+if not exist Q:\ if not %tdrive%==q set vdrive=q
+if not %vdrive%==none goto :foundvdrive
 echo.
-echo Error: the file imagex.exe is missing from your USB stick's \windows\system32
+echo Error:  I need a vhd drive letter, but could not find one between 
+echo Q: and W:.  I can't do the job without a free drive letter, so I've got 
+echo to stop.
+echo.
+goto :badend
+
+:foundvdrive
+REM
+REM Then check for an onboard copy of imagex and Dism.
+REM
+if exist x:\windows\system32\imagex.exe ((set osversion=7)&(goto :drivesok))
+if exist x:\windows\system32\Dism.exe ((set osversion=10)&(if not exist %tdrive%\scratch mkdir %tdrive%\scratch)&(goto :drivesok))
+echo.
+echo Error: imagex.exe and Dism.exe are missing from your USB stick's \windows\system32
 echo folder.  Please rebuild your Steadier State install USB stick or CD ISO with
 echo buildpe.cmd and use that new device to boot this system and try again.
 echo.
 goto :badend
 
-:foundimagex
+:drivesok
 REM
 REM Create two diskpart scripts.
-REM The first will wipe Drive 0 on the system, REM make a 1GB partition, format it, 
+REM The first will wipe Drive 0 on the system, make a 1GB partition, format it, 
 REM and assign the temporary drive letter to it.
 REM The second will create a partition from the remaining space and make it C:,
 REM rearranging the C: drive letter if it's currently being used.
 REM 
 echo select disk 0 >%drive%\wiperb.txt
 echo clean >>%drive%\wiperb.txt
-echo cre par pri size=1000  >> %drive%\wiperb.txt
+echo create partition primary size=1000  >> %drive%\wiperb.txt
 echo active>>%drive%\wiperb.txt
 echo format fs=ntfs quick label="System Reserved">>%drive%\wiperb.txt
 echo assign letter=%tdrive%>>%drive%\wiperb.txt
 echo rescan >>%drive%\wiperb.txt
 echo exit>>%drive%\wiperb.txt
-
 REM
 REM wiperc.txt is phase two if there's currently a C:
 REM
-echo sel vol c >%drive%\wiperc.txt
+echo select volume c >%drive%\wiperc.txt
 echo assign >>%drive%\wiperc.txt
 echo select disk 0 >>%drive%\wiperc.txt
-echo cre par pri >> %drive%\wiperc.txt
-echo format fs=ntfs quick label="Physical Drive">>%drive%\wiperc.txt
-echo assign letter=c>>%drive%\wiperc.txt
-echo exit>>%drive%\wiperc.txt
-
+echo create partition primary >>%drive%\wiperc.txt
+echo format fs=ntfs quick label="Physical Drive" >>%drive%\wiperc.txt
+echo assign letter=c >>%drive%\wiperc.txt
+echo exit >>%drive%\wiperc.txt
+REM
 REM wipernoc.txt is phase two if there's NOT currently a C:
-REM 
-
+REM
 echo select disk 0 >%drive%\wipernoc.txt
-echo cre par pri >> %drive%\wipernoc.txt
-echo format fs=ntfs quick label="Physical Drive">>%drive%\wipernoc.txt
-echo assign letter=c>>%drive%\wipernoc.txt
-echo exit>>%drive%\wipernoc.txt
-
-
-
-
-
-rem
-rem with that done, give tdrive its colon
+echo create partition primary >>%drive%\wipernoc.txt
+echo format fs=ntfs quick label="Physical Drive" >>%drive%\wipernoc.txt
+echo assign letter=c >>%drive%\wipernoc.txt
+echo exit >>%drive%\wipernoc.txt
+REM
+REM with that done, give tdrive its colon
 set tdrive=%tdrive%:
 REM
 REM Warnings
@@ -154,10 +172,9 @@ echo word in this paragraph and then press Enter to start the wipe-and-rebuild
 echo process. (The 4-letter word starts with a "w.") Or type anything else 
 echo and press Enter to stop the process.
 echo.
-set response=
-set /p response=Please type the word in lowercase and press Enter.
+set /p wiperesponse=Please type the word in lowercase and press Enter.
 echo.
-if not s%response%==swipe ((echo Exiting.) & (goto :goodend))
+if not %wiperesponse%==wipe ((echo Exiting.)&(goto :goodend))
 cls
 echo ===============================================================================
 echo STEP ONE:  FORMAT AND PARTITION DRIVE ZERO
@@ -174,23 +191,19 @@ REM	you want to wipe and rebuild drive 0 in "list drive" in diskpart
 REM	You are running this batch file from the root of your USB stick/CD
 REM	I can set the new drive to tdrive:, that tdrive: is unused
 REM
-
-REM
 REM wipe partitions on 0, build a new 1GB one that's active, give it drive letter %tdrive%
 REM
 diskpart /s %drive%\wiperb.txt
-set dr1=%errorlevel%
-if %dr1%==0 ((echo Diskpart phase 1 ended successfully, we now have a System Reserved)&(echo partition.  Checking to see that the large partition will have drive letter C:)  &(goto :dphase2))
+set dispart1rc=%errorlevel%
+if %dispart1rc%==0 ((echo Diskpart phase 1 ended successfully, we now have a System Reserved)&(echo partition.  Checking to see that the large partition will have drive letter C:)&(goto :diskpart1ok))
 echo.
-echo Diskpart phase 1 failed, return code %dr1%.
+echo Diskpart phase 1 failed, return code %dispart1rc%.
 echo It's not really safe to continue so I'm stopping here.  Look at what Diskpart
 echo just reported to see if there's a clue in there.  You may also get a clue from
 echo the diskpart scripts (wiperb.txt, wiperc.txt, wipernoc.txt) on drive %drive%.
-
 goto :eof
 
-:dphase2
-
+:diskpart1ok
 REM
 REM tried bootsect %tdrive% /nt60 [/mbr] and no help there with multidrive scenarios 
 REM
@@ -200,18 +213,16 @@ REM in disk 0 won't be C: for the rest of this run, and bcd's screwed up
 REM among other things.  If diskpart were more automatable, I could fix that
 REM by temporarily re-lettering anything that's currently C:, but it isn't.
 REM
-
 set noc=true
-if exist c:\ ((set noc=false) & (echo C: exists, we'll have to rearrange drive letters.))
-
+if exist c:\ ((set noc=false)&(echo C: exists, we'll have to rearrange drive letters.))
 echo.
 echo Running Diskpart phase 2.  We'll create the large partition, letter it C:
 echo and give it a label of "Physical Drive."  That'll be the drive you'll copy
 echo an image.vhd onto.  Here goes...
 echo.
-if %noc%==true ((diskpart /s %drive%\wipernoc.txt) & (set dr2=%errorlevel%))
-if %noc%==false ((diskpart /s %drive%\wiperc.txt) & (set dr2=%errorlevel%))
-if %dr2%==0 goto :dp2ok
+if %noc%==true ((diskpart /s %drive%\wipernoc.txt)&(set diskpart2rc=%errorlevel%))
+if %noc%==false ((diskpart /s %drive%\wiperc.txt)&(set diskpart2rc=%errorlevel%))
+if %diskpart2%==0 goto :diskpart2ok
 echo.
 echo Diskpart phase 2 failed.  Take a look at the Diskpart output to get
 echo any clues about why it failed and try again.  The most common problem arises
@@ -224,14 +235,11 @@ echo You may also get a clue from the diskpart scripts (wiperb.txt, wiperc.txt,
 echo wipernoc.txt) on drive %drive%.
 echo
 goto :eof
-:dp2ok
+
+:diskpart2ok
 echo.
 echo Diskpart phase 2 successfuly completed.
 echo Large drive formatted and labeled C:.
-echo.
-
-
-REM
 echo.
 echo =============================================================================
 echo STEP TWO:  Install WinPE on System Reserved Partition
@@ -243,43 +251,107 @@ echo AND -- bonus! -- serves as a "maintenance" copy of Windows that's very usef
 echo for resolving various boot and storage problems.
 echo =============================================================================
 echo.
+REM
 REM Now image the boot.wim from the PE drive to the new T:
 REM
+if %osversion%==7 (
 imagex /apply %realdrive%\sources\boot.wim 1 %tdrive% /check /verify
-set irc=%errorlevel%
-if %irc%==0 goto :imagex1ok
+) else (
+Dism /Apply-Image /ImageFile:%realdrive%\sources\boot.wim /ApplyDir:%tdrive% /ScratchDir:%tdrive%\scratch /Index:1 /CheckIntegrity /Verify
+)
+set applyrc=%errorlevel%
+if %applyrc%==0 goto :applyok
 echo.
-echo ERROR:  ImageX failed with return code %irc%.  Can't continue, exiting.
+echo ERROR: Failed to apply the image with return code %applyrc%.  Can't continue, exiting.
 goto :eof
-:imagex1ok
-echo ImageX successfully imaged boot.wim onto %tdrive%.
+
+:applyok
+if %osversion%==7 echo ImageX successfully imaged boot.wim onto %tdrive%.
+if %osversion%==10 echo Dism successfully imaged boot.wim onto %tdrive%.
 echo.
 echo ===============================================================
 echo STEP THREE: Copy Boot Files to System Reserved Partition
 echo.
 echo With that out of the way, we'll need some extra boot files that
 echo do not ship with WinPE, so we'll copy them from your existing
-echo WinPE device with Robocopy to the System Reserved partition.
+if %osversion%==7 echo WinPE device with Robocopy to the System Reserved partition.
+if %osversion%==10 echo WinPE device with BCDBoot to the System Reserved partition.
 echo ===============================================================
 echo.
 REM
 REM Grab a basic boot folder and BOOTMGR
 REM
-
+if %osversion%==7 (
 robocopy %realdrive%\boot %tdrive%\boot * /e /a-:ar
-set rcp1=%errorlevel%
-if %rcp1%==1 goto :rp1ok
+set robocopy1rc=!errorlevel!
+if %robocopy1rc%==0 ((copy %realdrive%\bootmgr %tdrive% /y)&(goto :bcdcopyok))
 echo.
-echo ERROR:  Robocopy failed with return code %rcp1%.  Can't continue, exiting.
+echo ERROR:  Robocopy failed with return code %robocopy1rc%.  Can't continue, exiting.
 goto :eof
-:rp1ok
-
-copy %realdrive%\bootmgr %tdrive% /y
-
+) else (
 REM
-REM the current BCD is of no value, so next we'll delete it and build a new
-REM one from scratch
+REM Attach the vhd
+REM attachvhd.txt is the name of the script attach the vhd
 REM
+echo select vdisk file=C:\image.vhd >%drive%\attachvhd.txt
+echo attach vdisk >>%drive%\attachvhd.txt
+echo exit >>%drive%\attachvhd.txt
+diskpart /s %drive%\attachvhd.txt
+set diskpart3rc=!errorlevel!
+if !diskpart3rc!==0 ((echo Diskpart phase 3 ended successfully, vhd was mounted.)&(goto :listvolume))
+echo.
+echo Diskpart phase 3 failed, return code !diskpart3rc!.
+echo It's not really safe to continue so I'm stopping here.  Look at what Diskpart
+echo just reported to see if there's a clue in there.  You may also get a clue from
+echo the diskpart scripts (wiperb.txt, wiperc.txt, wipernoc.txt, attachvhd.txt,
+echo mountvhd.txt, listvhd.txt) on drive %drive%.
+goto :eof
+
+:listvolume
+REM
+REM listvhd.txt is the name of the script to find the volumes
+REM
+echo list volume >>%drive%\listvhd.txt
+echo exit >>%drive%\listvhd.txt
+for /f "tokens=2,4" %%a in ('diskpart /s %drive%\listvhd.txt') do (if %%b==Windows_SRS set volnum=%%a)
+set volnumrc=!errorlevel!
+if !volnumrc!==0 ((echo Diskpart phase 4 ended successfully, vhd is volume !volnum!.)&(goto :foundvolume))
+echo.
+echo Diskpart phase 4 failed, return code !volnumrc!.
+echo It's not really safe to continue so I'm stopping here.  Look at what Diskpart
+echo just reported to see if there's a clue in there.  You may also get a clue from
+echo the diskpart scripts (wiperb.txt, wiperc.txt, wipernoc.txt, attachvhd.txt,
+echo mountvhd.txt, listvhd.txt) on drive %drive%.
+goto :eof
+
+:foundvolume
+REM
+REM mountvhd.txt is the name of the script to assign the drive letter
+REM
+echo select volume !volnum! >%drive%\mountvhd.txt
+echo assign letter=%vdrive% >>%drive%\mountvhd.txt
+echo exit >>%drive%\mountvhd.txt
+diskpart /s %drive%\mountvhd.txt
+set diskpart4rc=!errorlevel!
+if !diskpart4rc!==0 (set vdrive=%vdrive%:)&(echo Diskpart phase 5 ended successfully, vhd is drive !vdrive!.)&(goto :vhdok))
+echo.
+echo Diskpart phase 5 failed, return code %diskpart4rc%.
+echo It's not really safe to continue so I'm stopping here.  Look at what Diskpart
+echo just reported to see if there's a clue in there.  You may also get a clue from
+echo the diskpart scripts (wiperb.txt, wiperc.txt, wipernoc.txt, attachvhd.txt,
+echo mountvhd.txt, listvhd.txt) on drive %drive%.
+goto :eof
+
+:vhdok
+bcdboot !vdrive!\windows /s %tdrive%
+set bcdbootrc=!errorlevel!
+if !bcdbootrc!==0 goto :bcdcopyok
+echo.
+echo ERROR:  BCDBoot failed with return code !bcdbootrc!.  Can't continue, exiting.
+goto :eof
+)
+
+:bcdcopyok
 echo.
 echo ==============================================================================
 echo STEP FOUR:  BUILD A NEW BOOT CONFIGURATION DATABASE
@@ -287,47 +359,61 @@ echo.
 echo Now we'll need a new boot configuration database (BCD).  It's an essential
 echo file that every copy of Windows since Vista requires, and we need one that
 echo knows how to boot WinPE from your hard disk's System Reserved partition.
-echo We do that with a dozen "bcdedit" commands.  First, though, we delete the
-echo existing BCD.
+if %osversion%==7 echo We do that with a dozen "bcdedit" commands.  First, we delete the existing BCD.
+if %osversion%==10 echo We do that with a dozen "bcdboot" and "bcdedit" commands.
 echo.
-echo You should see eleven responses that look like echo "The operation completed
-echo successfully" with one response of "The entry {bootmgr} was 
-echo successfully created."
+echo You should see a series of responses that indicate they were completed
+echo successfully. If they do not all complete successfully something went
+echo wrong.
 echo ========================================================
 echo.
-del %tdrive%\boot\bcd 
 %tdrive%
-
+set guid=
+if %osversion%==7 (
+REM
+REM the current BCD is of no value, so next we'll delete it and build a new
+REM one from scratch
+REM
+del %tdrive%\boot\bcd 
 REM
 REM The annoying part about this is that we can't just build a new BCD
 REM We've got to build an offline BCD, then import it to the real BCD
 REM
-set guid=
 md \temp
 cd \temp
 echo on
-bcdedit /createstore bcd  
-bcdedit /store bcd -create {bootmgr} /d "Boot Manager" 
-bcdedit /store bcd -set {bootmgr} device boot 
-for /f "tokens=2 delims={}" %%i in ('bcdedit /store bcd /create /d "Roll Back Windows" -application osloader') do (set guid={%%i%})
+bcdedit /createstore bcd
+set "bcdstore=/store bcd"
+bcdedit !bcdstore! -create {bootmgr} /d "Boot Manager"
+bcdedit !bcdstore! -set {bootmgr} device boot
+) else (
+set "bcdstore=%tdrive%\EFI\Microsoft\Boot\BCD"
+)
+for /f "tokens=2 delims={}" %%i in ('bcdedit %bcdstore% /create /d "Roll Back Windows" -application osloader') do (set guid={%%i%})
 @echo off
 REM
-REM NOW we can import the bcd and knock off the "/store bcd" stuff 
+REM Windows 7 can import the bcd and knock off the "/store bcd" stuff
+REM Windows 10 will still need the /store parameter
 REM
-echo on
+if %osversion%==7 (
+set 'bcdstore'=''
 bcdedit /import bcd 
 cd \ 
 rd \temp /s /q
-bcdedit /set %GUID% osdevice partition=%tdrive%
-bcdedit /set %GUID% device partition=%tdrive% 
-bcdedit /set %GUID% path \windows\system32\boot\winload.exe 
-bcdedit /set %GUID% systemroot \windows 
-bcdedit /set %GUID% winpe yes
-bcdedit /set %GUID% detecthal yes 
-bcdedit /displayorder %GUID% /addlast 
-bcdedit /timeout 15 
+)
+echo on
+bcdedit %bcdstore% /set %GUID% osdevice partition=%tdrive%
+bcdedit %bcdstore% /set %GUID% device partition=%tdrive% 
+if %osversion%==7 bcdedit /set %GUID% path \windows\system32\boot\winload.exe
+if %osversion%==10 bcdedit %bcdstore% /set %GUID% path \windows\system32\boot\winload.efi
+bcdedit %bcdstore% /set %GUID% systemroot \windows
+bcdedit %bcdstore% /set %GUID% winpe yes
+bcdedit %bcdstore% /set %GUID% detecthal yes 
+bcdedit %bcdstore% /displayorder %GUID% /addlast 
+bcdedit %bcdstore% /timeout 5 
 @echo off
-if exist %tdrive%\boot\bcd goto :bcdok
+if %osversion%==7 if exist %tdrive%\boot\bcd goto :bcdok
+if %osversion%==10 %tdrive%\EFI\Microsoft\Boot\BCD goto :bcdok
 echo.
 echo ++++++ BCD CREATION FAILURE +++++++++
 echo.
@@ -351,6 +437,7 @@ echo.
 echo Ending PREPNEWPC.
 echo.
 goto :eof
+
 :bcdok
 echo.
 echo ===============================================================================
@@ -366,19 +453,21 @@ echo.
 REM
 REM copy over the Steadier State files from the USB stick
 REM
-robocopy %drive%\sdrstate %tdrive%\sdrstate
+robocopy %drive%\srs %tdrive%\srs
 REM
 REM and the updated startnet.cmd
 REM
 copy %drive%\startnethd.cmd %tdrive%\windows\system32\startnet.cmd /y
-
-copy %drive%\windows\system32\imagex.exe %tdrive%\windows\system32 /y
-
+if %osversion%==7 copy %drive%\windows\system32\imagex.exe %tdrive%\windows\system32 /y
+if %osversion%==10 copy %drive%\windows\system32\Dism.exe %tdrive%\windows\system32 /y
+REM
 REM Change the background wallpaper to winpe1.bmp, showing it's a HD boot
-
-copy %tdrive%\sdrstate\winpe1.bmp %tdrive%\windows\system32\winpe.bmp /y
+REM
+copy %tdrive%\srs\winpe1.bmp %tdrive%\windows\system32\winpe.bmp /y
+REM
 REM done with it, delete
-del %tdrive%\sdrstate\winpe1.bmp
+REM
+del %tdrive%\srs\winpe1.bmp
 REM
 echo.
 echo.
@@ -403,6 +492,8 @@ echo   a look in the documentation about using the "merge" command.
 echo.
 echo I hope you find this useful! 
 echo -- Mark Minasi help@minasi.com www.steadierstate.com
+echo This copy of SteadierState has been modified and the source
+echo can be found at https://github.com/7heMC/SteadierState
 echo.
 goto :goodend
 
@@ -429,8 +520,8 @@ echo.
 echo.
 goto :badend 
 
-
 :badend
 goto :eof
+
 :goodend
 goto :eof
