1c9a2152c9118f49d6cec7089440dd9ca937e61c
7heMC <mcecildietz@gmail.com>
2017-07-28 06:34:37 -0400

    Create nightlyrestart.cmd and nightlymerge.cmd
    These two commands are used to automatically restart the machine each and merge any changes.
    First restart is to undo all changes and revert back to the base image. The second restart occurs two hours later after the computer has had a chance to process any potential updates, etc. The third restart occurs another two hours later simply to ensure that the machine has finished processing any additional automated commands.
    The times can be changed to whatever works in your environment.

--- firstrun.cmd
+++ firstrun.cmd
@@ -16,10 +16,18 @@
 	
 :restarttasks
 	rem
-	rem Create a few tasks to reboot the computer every night
-	rem We found this necessary to maintain the computer's
-	rem relationship with the domain.
+	rem Create a few commands and tasks to reboot the computer
+	rem every night. We found this necessary to maintain the
+	rem computer's relationship with the domain, as well as
+	rem facilitate updates, especially Windows Updates.
 	rem
+	echo.
+	echo Creating nightlyreset.cmd
+	echo shutdown /r /t 0 > %systemdrive%\srs\nightlyreset.cmd
+	echo.
+	echo Creating nightlymerge.cmd
+	echo. > %systemdrive%\srs\automerge.txt
+	echo shutdown /r /t 0 > %systemdrive%\srs\nightlymerge.cmd
 	echo.
 	echo Creating a few tasks to reboot the computer every night
 	schtasks /Create /RU "NT AUTHORITY\SYSTEM" /SC DAILY /TN nightlyrestart0 /TR %systemdrive%\srs\nightlyreset.cmd /ST 01:00 /F
